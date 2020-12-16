19d03d18ccfa5c25f8a14b43dc3acf8a80e3c2fe
ihollander <ianholla@gmail.com>
2020-12-16 09:20:31 -0500

    Updated starter code

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -4,7 +4,6 @@ function CategoryFilter() {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      <button className="selected">All</button>
       {/* render <button> elements for each category here */}
     </div>
   );
