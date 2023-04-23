50069831db10cbb0701518279d52f3f2b48d1451
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-23 12:59:15 -0700

    added task list

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,7 @@ import React from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
   return (
@@ -13,7 +10,7 @@ function App() {
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={TASKS} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
+    <ul className="tasks">
+      {tasks.map(task => (
+        <Task key={task.id} task={task} />
+      ))}
+    </ul>
   );
 }
 
